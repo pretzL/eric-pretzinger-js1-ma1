5951a935631adc5c29defb7c05106cbc66f5b55c
pretzL <ericpretzinger@gmail.com>
2022-01-26 22:11:15 +0100

    fixed small issue on q1 where q1 was set up as an array with one object, is now just one object

--- js/script.js
+++ js/script.js
@@ -16,13 +16,11 @@ const cats = [
 Create an object called cat.
 Give the object one property called complain. complain's value should be a method (a function) which logs the string "Meow!".*/
 
-const cat = [
-  {
-    complain: function () {
-      console.log("Meow!");
-    },
+const cat = {
+  complain: function () {
+    console.log("Meow!");
   },
-];
+};
 
 /* Question 2:
 Select the h3 from the HTML using the querySelector method and assign it to a variable called heading.
